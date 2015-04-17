3d09a9859dd25023b92fa79b29b88b8d9c2a0ac4
GeneralSarsby <matt.sarsby@gmail.com>
2015-04-17 16:22:53 +0100

    Update asciiIcons.js
    Changed the EOL symbol to '\n' from 
    `
    `
    as Safari on Mac and iOS dosn't have string templates.
    Should be a non-breaking change.

--- asciiIcons.js
+++ asciiIcons.js
@@ -173,5 +173,4 @@ CanvasRenderingContext2D.prototype.drawIcon = function(text,x,y,w){
 CanvasRenderingContext2D.prototype.iconSymbols = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 CanvasRenderingContext2D.prototype.iconAutoLineWidth = true;
 CanvasRenderingContext2D.prototype.iconDebug = false;
-CanvasRenderingContext2D.prototype.iconEOLSymbol = `
-`;
+CanvasRenderingContext2D.prototype.iconEOLSymbol = '\n';
